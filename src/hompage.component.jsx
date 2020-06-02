f1984f4f49147daf455022d1e402098c4c63f044
Marko Gusic <markogusic@Markos-MacBook-Pro.local>
2020-06-01 22:47:36 -0700

    first push

---
+++ src/hompage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">HATS</h1>
+                    <span className="subtittle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">JACKETS</h1>
+                    <span className="subtittle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">SNEACKERS</h1>
+                    <span className="subtittle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">WOMENS</h1>
+                    <span className="subtittle">SHOP NOW</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="tittle">MENS</h1>
+                    <span className="subtittle">SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
